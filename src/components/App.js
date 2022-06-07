21089bfb31cf97b9b6d7f5bc5ed57e3dd784ead4
James <jamesthao32@gmail.com>
2022-06-06 21:44:01 -0600

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,27 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [selectedId, setSelectedId] = useState("All")
+  const [Tasks, setTask] = useState(TASKS);
+  const handleSelect = (item) => {
+    let updatedTask = Tasks.filter(i => i.category === item || item === "All")
+    setSelectedIF(() => item) {
+    setTask(() => updatedTask)
+    }
+  }
+  const onTaskFormSubmit = (item) => {
+    setTask((Tasks) => [...Task, item])
+  }
+  const onDelete = (id) => {
+      const updatedTask =Tasks.filter((_, i) => i !== id)
+      setTask(() => updatedTask)
+  }  
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} selectedId={selectedId} handleSelect={handleSelect} />
+      <NewTaskForm onTaskFormSubmit={onTaskFormSubmit} categories={CATEGORIES} />
+      <TaskList selectedId={selectedId} tasks={Tasks} onDelete={onDelete} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, onDelete }) {
+  let data = tasks ? tasks : []
   return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
-}
+    <div className="tasks"></div>
 
-export default TaskList;
+    {data.map((item, i) =>
+      <Task
+        onDelete={() => onDelete(i)}
+        key={i} text={item.text} category={item.category}
+      />
+    )}
+
+    );
+  }
+  export default TaskList;``
